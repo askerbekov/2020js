beea92ebfec7e88af4e3ab91b1f2bb7ac56ad6d1
Mederbek Askerbekov <askerbekov@outlook.com>
2020-01-24 14:41:07 +0600

    add js

---
+++ script.js
@@ -0,0 +1,9 @@
+let gradient = document.querySelector("#gradient");
+let color = document.querySelector("#color");
+
+let color2 = document.querySelector("#color2");
+let button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  gradient.style;
+});
